cb3caf3f684f28f966e901f38138f22d61a7ac14
unknown <apelaezia@insjoaquimmir.cat>
2025-05-31 19:44:17 +0200

    Backend and frontend deployment

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('TrainR backend server is running.');
+});
